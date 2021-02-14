27b90f5934f6e55df78627e85e0b1cf63beb8d7c
teoyi <yipengteo@yahoo.com.sg>
2021-02-13 22:13:57 -0800

    Added clear, setKey values, getKey index functionality to gameBoard factory

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,48 @@
 const gameBoard = (() => {
     // Creating a board with an array of 9 spaces 
     let _board = new Array(9); 
-    const keys = document.getElementsByClassName('btn');
-    // Connecting elements of the array to the grid button of respectively
+    const _keys = document.getElementsByClassName('btn');
+
+    // Connecting elements of the array to the grid button value
     for (let i=0; i<9; i++) {
-        _board[i] = keys[i] 
+        _board[i] = _keys[i].innerHTML 
     };
+
     // Getting the values of the squares of interest 
     const getKey = (index) => _board[index];
-    // // Setting values for the squares of interest 
-    // const setKey = (index, player) => { 
-    //     const btn = document.getElementsByClassname('btn');
-    //     btn.addEventListener("click", function() {
-    //         _board[index].innerHTML = player.getRole();
-    //     });
-    // }
+
+    // Setting values for the squares of interest 
+    const setKey = (index, player) => { 
+        // Update array for square of interest 
+        _board[index] = player.getRole(); // .getRole() will be added to determine player's decision on 'x' or 'o'
+        _keys[index].innerHTML = _board[index] //Replacing button's html with the correct 'x' or 'o'
+    }
+
+    const clear = () => {
+        for (let i=0; i<_board.length; i++){
+            _board[i] = '';
+            _keys[i].innerHTML = '';
+        }
+    }
     
+    return{ 
+        getKey,
+        setKey,
+        clear
+    };
 
 })();
 
 keys = document.getElementsByClassName('btn');
 arr = new Array(9)
 console.log(arr)
-
 for (let i=0; i <9; i++) {
     arr[i] = keys[i].innerHTML 
 }
+console.log(arr)
+
+keys[0].innerHTML = 'o';
+arr[0] = keys[0].innerHTML;
 console.log(arr)
 // console.log(typeof(keys[0]));
 // console.log(keys);
