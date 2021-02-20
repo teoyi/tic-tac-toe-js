b160115ad091d9c5673201c1199cb9fa9210c7b0
teoyi <yipengteo@yahoo.com.sg>
2021-02-19 21:48:10 -0800

    Added setRole function to add active class into role buttons

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const gameBoard = (() => {
     // Creating a board with an array of 9 spaces 
     let _board = new Array(9); 
     const _keys = document.getElementsByClassName('btn');
+  
 
     // Connecting elements of the array to the grid button value
     for (let i=0; i<9; i++) {
@@ -16,13 +17,22 @@ const gameBoard = (() => {
     // Setting values for the squares of interest 
     const setKey = (index, player) => { 
         if (_board[index]) {
-            alert('This position is already taken. Try again!')
+            alert('This position is already taken. Try again!');
         } else {
             // Update array for square of interest 
         _board[index] = player //For now, player will be manually palced for a role
         //_board[index] = player.getRole(); // .getRole() will be added to determine player's decision on 'x' or 'o'
         _keys[index].innerHTML = _board[index] //Replacing button's html with the correct 'x' or 'o'
         } 
+    }
+
+    const bind = () => {
+        let player = 'x';
+        for (let i=0; i<9; i++) {
+            _keys[i].addEventListener('click', function(){
+                
+            })
+        }
     }
 
     const clear = () => {
@@ -40,13 +50,8 @@ const gameBoard = (() => {
 
 })();
 
-gameBoard.setKey(0, 'x')
-gameBoard.setKey(2, 'o')
-gameBoard.setKey(4, 'x')
-gameBoard.setKey(5, 'x')
-gameBoard.setKey(7, 'x')
-gameBoard.setKey(8, 'o')
-gameBoard.setKey(8, 'x')
+
+// gameBoard.setKey(8, 'x')
 
 
 // keys = document.getElementsByClassName('btn');
@@ -65,36 +70,43 @@ gameBoard.setKey(8, 'x')
 // arr[1] = keys[1].innerHTML;
 // console.log(arr)
 
-
+// let roleX = document.getElementById('x');
+// roleX.classList.add("active");
 // Player factory need some work 
 const Player = (role) => {
     let _role = role; 
+
+    const initRole = (role) => {
+        _role = role;
+        if (_role == 'x') {
+            roleO.classList.remove("active");
+            roleX.classList.add("active");
+        } else {
+            roleX.classList.remove("active");
+            roleO.classList.add("active");
+        }
+    }
     const getRole = () => _role; 
-    const setRole = () => (role, active) => {
-        _role = role; 
-        roleX = document.getElementById('x');
-        roleO = document.getElementById('o');
-        roleX.addEventListener("click", function() {
-            if (this.classList.contains("active")) {
-                this.classList.remove("active");
-                console.log('removed!')
-            } else {
-                this.classList.add("active");
-                roleO.classList.remove("active");
-                console.log('added!')
-            };
-        });
-        // roleO.addEventListener("click", function() {
-        //     if (this.classList.contains("active")) {
-        //         this.classList.remove("active");
-        //     } else {
-        //         this.classList.add("active");
-        //         roleX.classList.remove("active");
-        //     };
-        // });
+    const setRole = () => {
+        if (document.getElementById(_role).classList.contains('active')){
+
+        } else (document.getElementById(_role).classList.add('active'));
     };
+
     return {
         getRole,
-        setRole
+        setRole,
+        initRole
     };
 };
+
+Luke = Player('x');
+Luke.setRole();
+Luke.getRole();
+
+gameBoard.setKey(0, 'x')
+gameBoard.setKey(2, 'o')
+gameBoard.setKey(4, 'x')
+gameBoard.setKey(5, 'x')
+gameBoard.setKey(7, 'x')
+gameBoard.setKey(8, 'o')
