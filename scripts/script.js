8f77a60677b68f2a27ce55c1dac90cde26066140
teoyi <yipengteo@yahoo.com.sg>
2021-02-21 23:01:06 -0800

    Added win condition array

--- scripts/script.js
+++ scripts/script.js
@@ -87,6 +87,20 @@ const gameController = (() => {
     const _roleO = document.getElementById('o');
     const _keys = document.getElementsByClassName('btn');
 
+    const winCondition = [
+                        [0,1,2], [3,4,5,],[6,7,8],
+                        [0,3,6],[1,4,7],[2,5,8],
+                        [0,4,8],[2,4,6]
+                    ]
+    
+    const checkWin = (board) => {
+        for (let i=0; i<9; i++){
+            for (let j=0; i<3; i++){
+                if (winCondition)
+            }
+        }
+    }
+
     const _init = (() => {
         _player1.setRole();
         for (let i=0; i<9; i++){
